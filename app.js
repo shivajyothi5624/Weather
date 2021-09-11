76ee318c86f78b42001545d59500e8050d033d8d
shiva <shivajyoth570@gmail.com>
2021-09-11 17:32:38 +0530

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 80;
+const port = process.env.PORT || 80;
 const static_path = path.join(__dirname,"./public")
 app.use(express.static(static_path ))
 app.set('view engine', 'hbs');
